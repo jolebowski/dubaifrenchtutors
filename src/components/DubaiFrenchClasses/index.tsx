2c1ecf1c75937a245c5b671d1d98030686327be0
Jordan Moreira <jordan.moreira@epitech.eu>
2024-09-08 17:27:18 +0200

    update DubaiFrenchClasses

--- src/components/DubaiFrenchClasses/index.tsx
+++ src/components/DubaiFrenchClasses/index.tsx
@@ -17,24 +17,24 @@ const DubaiFrenchClasses = ({ page }: { page: PageData }) => {
   const images = [
     {
       id: 1,
-      imageUrl: '/test.png',
+      imageUrl: 'https://cambridge-club.kyiv.ua/wp-content/themes/cambridge/images/head_img.png',
       text: 'de 18 à 99 ans COURS DE FRANÇAIS POUR ADULTES',
       linkUrl: 'FrenchCoursesAdults',
     },
-    // {
-    //   id: 2,
-    //   imageUrl:
-    //     'https://cambridge-club.kyiv.ua/wp-content/themes/cambridge/images/head_img.png',
-    //   text: 'de 6 à 12 ans COURS DE FRANÇAIS POUR ENFANTS',
-    //   linkUrl: '/image2-details',
-    // },
-    // {
-    //   id: 3,
-    //   imageUrl:
-    //     'https://cambridge-club.kyiv.ua/wp-content/themes/cambridge/images/head_img.png',
-    //   text: 'de 12 à 17 ans COURS DE FRANÇAIS POUR ADOLESCENTS',
-    //   linkUrl: '/image3-details',
-    // },
+    {
+      id: 2,
+      imageUrl:
+        'https://cambridge-club.kyiv.ua/wp-content/themes/cambridge/images/head_img.png',
+      text: 'de 6 à 12 ans COURS DE FRANÇAIS POUR ENFANTS',
+      linkUrl: '/image2-details',
+    },
+    {
+      id: 3,
+      imageUrl:
+        'https://cambridge-club.kyiv.ua/wp-content/themes/cambridge/images/head_img.png',
+      text: 'de 12 à 17 ans COURS DE FRANÇAIS POUR ADOLESCENTS',
+      linkUrl: '/image3-details',
+    },
   ];
 
   return (
@@ -48,12 +48,11 @@ const DubaiFrenchClasses = ({ page }: { page: PageData }) => {
 
       <div className="flex flex-col items-center mt-8">
         {images.map((image) => {
-          const { imageUrl } = image;
-          console.log(imageUrl, 'imageUrl');
+          const { imageUrl, id , linkUrl, text} = image;
           return (
             <Link
-              href={image.linkUrl}
-              key={image.id}
+              href={linkUrl}
+              key={id}
               className="cursor-pointer"
             >
               <div className="flex flex-col items-center mb-4 cursor-pointer md:flex-row md:items-center coco">
@@ -61,12 +60,12 @@ const DubaiFrenchClasses = ({ page }: { page: PageData }) => {
                   src={imageUrl}
                   width={300}
                   height={200}
-                  alt={`Image ${image.id}`}
+                  alt={`Image - ${text}`}
                   className="object-cover w-64 h-64 mb-4 rounded-lg md:w-96 md:h-96 md:mb-0 md:mr-4"
                 />
                 <div className="flex items-center justify-center transition-transform duration-300 ease-in-out transform rounded-full rounded-courses w-44 h-44 hover:w-52 hover:h-52 hover:scale-105">
                   <p className="text-base text-center md:text-lg lg:text-xl">
-                    {image.text}
+                    {text}
                   </p>
                 </div>
               </div>
