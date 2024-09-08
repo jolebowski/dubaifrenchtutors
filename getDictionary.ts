93953d17ad9c45356f45899cde2e7f5e503f33c3
Jordan Moreira <jordan.moreira@epitech.eu>
2024-09-08 18:07:52 +0200

    fix getDictionary

--- getDictionary.ts
+++ getDictionary.ts
@@ -5,4 +5,14 @@ const dictionaries = {
   ar: () => import('@/dictionaries/ar.json').then((module) => module.default),
   fr: () => import('@/dictionaries/fr.json').then((module) => module.default),
 };
-export const getDictionary = async (locale: Locale) => dictionaries[locale]();
+
+export const getDictionary = async (locale: Locale) => {
+  const langCode = locale.split('-')[0].toLowerCase();
+  
+  if (!(langCode in dictionaries)) {
+    console.log(`Unsupported locale: ${locale}. Falling back to English.`);
+    return dictionaries.en();
+  }
+  
+  return dictionaries[langCode as keyof typeof dictionaries]();
+};
