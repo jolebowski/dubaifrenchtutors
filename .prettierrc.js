f507dacdc8c9ad5a2ef378d02126893983379c39
Jordan Moreira <jordan.moreira@epitech.eu>
2024-10-20 18:36:52 +0200

    update prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  singleQuote: true, // Utilisez des guillemets simples au lieu de doubles
-  trailingComma: 'all', // Ajoute une virgule finale dans les tableaux et objets
-  semi: true, // Ajoute des points-virgules en fin de ligne
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
   printWidth: 80,
   tabWidth: 2,
   jsxBracketSameLine: false,
   arrowParens: 'always',
   quoteProps: 'as-needed',
-  endOfLine: 'lf'
+  endOfLine: 'lf',
 };
